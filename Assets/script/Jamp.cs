8ef6ddab957ce43fd464acfc2069a06a378ac9e7
hmrskw <hmrskw@gmail.com>
2015-08-22 17:19:03 +0900

    パワーバー実装

--- Assets/script/Jamp.cs
+++ Assets/script/Jamp.cs
@@ -1,20 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Jamp : MonoBehaviour {
 	public GameObject human;
-	double power = 0;
+	float power = 0;
 	//double testpower = 0.0;
-
+	Slider _slider;
 	// Use this for initialization
 	void Start () {
-
+		_slider = GameObject.Find("Slider").GetComponent<Slider>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		/*ジャンプするときのパワーをSin波で決める*/
-		power = (Mathf.Sin (Time.frameCount) + 1) * 100;
+		power = (Mathf.Sin (Time.frameCount/10f) + 1) * 50;
 
 		//Debug.Log (power);
 		Debug.Log (human.transform.localPosition.y);
@@ -23,8 +24,9 @@ public class Jamp : MonoBehaviour {
 		{
 			if (Input.GetMouseButtonUp (0))
 			{
-				human.transform.GetComponent<Rigidbody> ().AddForce (new Vector3 (0, (int)power * 10, 0));
+				human.transform.GetComponent<Rigidbody> ().AddForce (new Vector3 (0, (int)power * 20, 0));
 			}
 		}
+		_slider.value = power;
 	}
 }
