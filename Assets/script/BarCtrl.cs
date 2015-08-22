a763a5b2b8c52f4fdfbf612b8564eddad390671c
hmrskw <hmrskw@gmail.com>
2015-08-22 15:52:56 +0900

    パワーバーできました

---
+++ Assets/script/BarCtrl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BarCtrl : MonoBehaviour {
+	public float power = 0;
+
+	Slider _slider;
+	// Use this for initialization
+	void Start () {
+		_slider = GameObject.Find ("Slider").GetComponent<Slider> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		_slider.value = power;
+	}
+}
