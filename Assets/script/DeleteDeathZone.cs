c0960dc5e091eb8a55e8ec5bbdb1dfd873d7602e
shoichi <b1595134@planet.kanazawa-it.ac.jp>
2015-08-22 16:22:45 +0900

    player fall

---
+++ Assets/script/DeleteDeathZone.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeleteDeathZone : MonoBehaviour
+{
+	/** 何かに触れたときをタグで判定 */
+	void OnTriggerStay(Collider other)
+	{
+		// 掃除機に吸い込まれたら *
+		if (other.gameObject.tag.Equals("DeathZone"))
+		{
+			Debug.Log("I touch death zone.");
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Assets/script/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+	private bool isControllable = false;
+	void Start ()
+	{
+		enableControl ();
+	}
+	
+	void Update ()
+	{
+		if(!isControllable)
+		{
+			return;
+		}
+
+		float movePower = this.gameObject.GetComponent<Rigidbody> ().mass;
+		Debug.Log ("mass["+movePower+"]");
+
+		if(Input.GetKey(KeyCode.A))
+		{
+			this.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(-1.0f*movePower, 0.0f, 0.0f));
+			Debug.Log("move left");
+		}else if(Input.GetKey(KeyCode.D))
+		{
+			this.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(movePower, 0.0f, 0.0f));
+			Debug.Log("move right");
+		}
+	}
+
+	/** enable to player control */
+	public void enableControl()
+	{
+		isControllable = true;
+	}
+
+
+}
